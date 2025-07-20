245cea997e4c24e357225c2832e06db7a2a0754c
xXnexioXx <78569707+xXnexioXx@users.noreply.github.com>
2025-07-20 03:21:49 +0200

    Update CritSound.lua

--- CritSound.lua
+++ CritSound.lua
@@ -1,64 +1,50 @@
 local f=CreateFrame("Frame","CritSoundFrame",UIParent,"BackdropTemplate")
 f:SetSize(250,150)f:SetPoint("CENTER")f:SetBackdrop({bgFile="Interface\\DialogFrame\\UI-DialogBox-Background",edgeFile="Interface\\DialogFrame\\UI-DialogBox-Border",tile=1,tileSize=32,edgeSize=32,insets={left=8,right=8,top=8,bottom=8}})
-f:EnableMouse(1)f:SetMovable(1)f:RegisterForDrag("LeftButton")f:SetScript("OnDragStart",f.StartMoving)f:SetScript("OnDragStop",f.StopMovingOrSizing)f:Hide()
+f:EnableMouse(true)f:SetMovable(true)f:RegisterForDrag("LeftButton")f:SetScript("OnDragStart",f.StartMoving)f:SetScript("OnDragStop",f.StopMovingOrSizing)f:Hide()
 
 CritSoundCharDB=CritSoundCharDB or{critNumber=420,sound="sound04.ogg"}
-local sounds={"sound01.ogg","sound02.ogg","sound03.ogg","sound04.ogg","sound05.ogg","sound06.ogg","sound07.ogg","sound08.ogg","sound09.ogg","sound10.ogg"}
+local s={"sound01.ogg","sound02.ogg","sound03.ogg","sound04.ogg","sound05.ogg","sound06.ogg","sound07.ogg","sound08.ogg","sound09.ogg","sound10.ogg"}
+local l=0
 
-local close=CreateFrame("Button",nil,f,"UIPanelCloseButton")close:SetPoint("TOPRIGHT",-5,-5)close:SetScale(0.8)
-local critLabel=f:CreateFontString(nil,"OVERLAY","GameFontNormal")critLabel:SetPoint("TOPLEFT",20,-30)critLabel:SetText("Min Crit:")
-local critInput=CreateFrame("EditBox",nil,f,"InputBoxTemplate")critInput:SetSize(70,20)critInput:SetPoint("LEFT",critLabel,"RIGHT",10,0)
-critInput:SetAutoFocus(false)critInput:SetNumeric(true)critInput:SetMaxLetters(6)
-local soundLabel=f:CreateFontString(nil,"OVERLAY","GameFontNormal")soundLabel:SetPoint("TOPLEFT",critLabel,"BOTTOMLEFT",0,-20)soundLabel:SetText("Sound:")
-local dropdown=CreateFrame("Frame",nil,f,"UIDropDownMenuTemplate")dropdown:SetPoint("LEFT",soundLabel,"RIGHT",-10,-2)UIDropDownMenu_SetWidth(dropdown,140)
+CreateFrame("Button",nil,f,"UIPanelCloseButton"):SetPoint("TOPRIGHT",-5,-5):SetScale(0.8)
 
-local setBtn=CreateFrame("Button",nil,f,"UIPanelButtonTemplate")setBtn:SetSize(70,22)setBtn:SetPoint("TOPLEFT",soundLabel,"BOTTOMLEFT",0,-20)setBtn:SetText("Set")
-local testBtn=CreateFrame("Button",nil,f,"UIPanelButtonTemplate")testBtn:SetSize(70,22)testBtn:SetPoint("LEFT",setBtn,"RIGHT",10,0)testBtn:SetText("Test")
-local critStatus=f:CreateFontString(nil,"OVERLAY","GameFontNormal")critStatus:SetPoint("BOTTOMLEFT",20,20)
-local soundStatus=f:CreateFontString(nil,"OVERLAY","GameFontNormal")soundStatus:SetPoint("BOTTOMRIGHT",-20,20)
+local c=f:CreateFontString(nil,"OVERLAY","GameFontNormal")c:SetPoint("TOPLEFT",20,-30)c:SetText("Min Crit:")
+local i=CreateFrame("EditBox",nil,f,"InputBoxTemplate")i:SetSize(70,20)i:SetPoint("LEFT",c,"RIGHT",10,0)i:SetAutoFocus(false)i:SetNumeric(true)i:SetMaxLetters(6)
 
-local function UpdateStatus()
-    critStatus:SetText("Crit: "..CritSoundCharDB.critNumber)
-    soundStatus:SetText("Sound: "..CritSoundCharDB.sound)
-end
+local sl=f:CreateFontString(nil,"OVERLAY","GameFontNormal")sl:SetPoint("TOPLEFT",c,"BOTTOMLEFT",0,-20)sl:SetText("Sound:")
+local dd=CreateFrame("Frame",nil,f,"UIDropDownMenuTemplate")dd:SetPoint("LEFT",sl,"RIGHT",-10,-2)UIDropDownMenu_SetWidth(dd,140)
 
-setBtn:SetScript("OnClick",function()
-    CritSoundCharDB.critNumber=math.min(math.max(tonumber(critInput:GetText())or 1000,1),999999)
-    UpdateStatus()
-end)
+local sb=CreateFrame("Button",nil,f,"UIPanelButtonTemplate")sb:SetSize(70,22)sb:SetPoint("TOPLEFT",sl,"BOTTOMLEFT",0,-20)sb:SetText("Set")
+local tb=CreateFrame("Button",nil,f,"UIPanelButtonTemplate")tb:SetSize(70,22)tb:SetPoint("LEFT",sb,"RIGHT",10,0)tb:SetText("Test")
 
-testBtn:SetScript("OnClick",function()
-    PlaySoundFile("Interface\\AddOns\\CritSound\\"..CritSoundCharDB.sound,"Master")
-end)
+local cs=f:CreateFontString(nil,"OVERLAY","GameFontNormal")cs:SetPoint("BOTTOMLEFT",20,20)
+local ss=f:CreateFontString(nil,"OVERLAY","GameFontNormal")ss:SetPoint("BOTTOMRIGHT",-20,20)
+
+local u=function()cs:SetText("Crit: "..CritSoundCharDB.critNumber)ss:SetText("Sound: "..CritSoundCharDB.sound)end
+sb:SetScript("OnClick",function()CritSoundCharDB.critNumber=math.min(math.max(tonumber(i:GetText())or 1000,1),999999)u()end)
+tb:SetScript("OnClick",function()PlaySoundFile("Interface\\AddOns\\CritSound\\"..CritSoundCharDB.sound,"Master")end)
 
 f:SetScript("OnShow",function()
-    critInput:SetText(CritSoundCharDB.critNumber)
-    UIDropDownMenu_Initialize(dropdown,function()
-        for _,sound in ipairs(sounds)do
+    i:SetText(CritSoundCharDB.critNumber)
+    UIDropDownMenu_Initialize(dd,function()
+        for _,v in ipairs(s)do
             local info=UIDropDownMenu_CreateInfo()
-            info.text=sound
-            info.func=function()CritSoundCharDB.sound=sound UIDropDownMenu_SetText(dropdown,sound)end
+            info.text=v
+            info.func=function()CritSoundCharDB.sound=v UIDropDownMenu_SetText(dd,v)end
             UIDropDownMenu_AddButton(info)
         end
     end)
-    UIDropDownMenu_SetText(dropdown,CritSoundCharDB.sound)
-    UpdateStatus()
+    UIDropDownMenu_SetText(dd,CritSoundCharDB.sound)
+    u()
 end)
-
-local lastSoundTime = 0
 
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent",function()
     local e={CombatLogGetCurrentEventInfo()}
-    local event,srcGUID,damage,crit=e[2],e[4],e[15],e[21]
-    if(srcGUID==UnitGUID("player")and crit and damage and damage>=CritSoundCharDB.critNumber and(
-        event=="SWING_DAMAGE"or event=="SPELL_DAMAGE"or event=="RANGE_DAMAGE"))then
-        local now = GetTime()
-        local cooldown = math.random(300,800)/1000 -- 0.3 bis 0.8 Sekunden
-        if now - lastSoundTime >= cooldown then
-            PlaySoundFile("Interface\\AddOns\\CritSound\\"..CritSoundCharDB.sound,"Master")
-            lastSoundTime = now
-        end
+    local ev,g,d,c=e[2],e[4],e[15],e[21]
+    if g==UnitGUID("player")and c and d and d>=CritSoundCharDB.critNumber and(ev=="SWING_DAMAGE"or ev=="SPELL_DAMAGE"or ev=="RANGE_DAMAGE")then
+        local t=GetTime()local cd=math.random(300,800)/1000
+        if t-l>=cd then PlaySoundFile("Interface\\AddOns\\CritSound\\"..CritSoundCharDB.sound,"Master")l=t end
     end
 end)
 
